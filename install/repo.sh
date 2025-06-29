3e6e63a945d1dbb89b7e76398e931cfc10fbc743
adfitzhu <adfitzhu@fitzworks.net>
2025-06-29 14:46:02 -0700

    added setup link to desktops after install

--- install/repo.sh
+++ install/repo.sh
@@ -1,2 +1,11 @@
 mkdir -p /mnt/usr/local
 git clone http://github.com/adfitzhu/nix /mnt/usr/local/nixos
+for userdir in /home/*; do
+    desktop_dir="$userdir/Desktop"
+    if [ -d "$userdir" ]; then
+        mkdir -p "$desktop_dir"
+        cp /mnt/usr/local/nixos/Setup.desktop "$desktop_dir/"
+        chown "$(basename "$userdir")":"$(basename "$userdir")" "$desktop_dir/Setup.desktop"
+        chmod 755 "$desktop_dir/Setup.desktop"
+    fi
+done
