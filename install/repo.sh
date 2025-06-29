9d196678f06282b5a74fffd78c6f53034747ce34
adfitzhu <adfitzhu@fitzworks.net>
2025-06-29 15:27:19 -0700

    trying setting ownership of desktop files

--- install/repo.sh
+++ install/repo.sh
@@ -1,12 +1,12 @@
 mkdir -p /mnt/usr/local
 git clone http://github.com/adfitzhu/nix /mnt/usr/local/nixos
 for userdir in /mnt/home/*; do
-    desktop_dir="/mnt/home/$userdir/Desktop"
+    desktop_dir=$userdir/Desktop"
     if [ -d "$userdir" ]; then
         username=$(basename "$userdir")
         mkdir -p "$desktop_dir"
         cp /mnt/usr/local/nixos/utils/Setup.desktop "$desktop_dir/"
-        chown "$username:$username" "$desktop_dir/Setup.desktop"
-        chmod 755 "$desktop_dir/Setup.desktop"
+        nixos-enter --root /mnt -- chown "$username" "/home/$username/Desktop/Setup.desktop"
+        nixos-enter --root /mnt -- chmod 755 "/home/$username/Desktop/Setup.desktop"
     fi
 done
