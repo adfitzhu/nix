aa92fe4d9d1bc850a047e2665ba33befe0fdac01
Adam Fitzhugh <adfitzhu@fitzworks.net>
2025-09-06 19:41:45 -0700

    added nixtop host

--- flake.nix
+++ flake.nix
@@ -86,7 +86,81 @@
           }
         ];
       };
-      
+
+     nixtop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ({ ... }: {
+            _module.args = {
+              autoUpgradeFlake = "github:adfitzhu/nix#nixtop";
+            };
+          })
+          ./base-config.nix
+          nix-flatpak.nixosModules.nix-flatpak
+          ({ pkgs, ... }: {
+            networking.hostName = "nixtop";
+            users.groups.adam = {};
+            users.users.adam = {
+              isNormalUser = true;
+              extraGroups = [ "networkmanager" "wheel" "vboxsf" "dialout" "audio" "video" "input" "docker" ];
+            };
+            environment.systemPackages = with pkgs; [
+            orca-slicer
+            ];
+            
+            # Flatpak packages for this host
+            services.flatpak.packages = [
+              "com.usebottles.bottles"
+              "com.heroicgameslauncher.hgl"
+              "com.discordapp.Discord"
+              "com.obsproject.Studio"
+              "com.github.tchx84.Flatseal"
+            ];
+            
+            services.xserver.enable = false;
+            services.displayManager = {
+              sddm.enable = true;
+              sddm.wayland.enable = true;
+              autoLogin = {
+                enable = true;
+                user = "adam";
+              };
+            };
+            programs.steam = {
+              enable = true;
+              remotePlay.openFirewall = true;
+              dedicatedServer.openFirewall = true;
+              localNetworkGameTransfers.openFirewall = true;
+            };
+
+            };
+            systemd.services.my-auto-upgrade = {
+              description = "Custom NixOS auto-upgrade (host-specific)";
+              serviceConfig.Type = "oneshot";
+              script = ''
+                set -euxo pipefail
+                ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --upgrade --flake github:adfitzhu/nix#nixtop --no-write-lock-file --impure
+              '';
+            };
+            systemd.timers.my-auto-upgrade = {
+              description = "Run custom NixOS auto-upgrade weekly (host-specific)";
+              wantedBy = [ "timers.target" ];
+              timerConfig = {
+                OnCalendar = "weekly";
+                Persistent = true;
+              };
+            };
+          })
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.adam = import ./home/adam/home.nix;
+          }
+        ];
+      };
+
       yactop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
